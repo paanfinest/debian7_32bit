8dd6410d24c361fec37aa80c5d330635cb9e62ba
paanfinest <paanfinest@gmail.com>
2017-07-04 14:42:16 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -90,6 +90,12 @@ wget https://github.com/dylanaraps/neofetch/blob/master/neofetch.1'
 mv neofetch /usr/bin/neofetch
 chmod +x /usr/bin/neofetch
 echo "clear" >> .profile
+
+# install neofetch
+echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | sudo tee -a /etc/apt/sources.list
+curl -L "https://bintray.com/user/downloadSubjectPublicKey?username=bintray" -o Release-neofetch.key && sudo apt-key add Release-neofetch.key && rm Release-neofetch.key
+apt-get update
+apt-get install neofetch
 echo "neofetch" >> .profile
 echo "clear" >> .bashrc 
 echo 'e "                 __   _,--="=--,_   __' >> .bashrc
