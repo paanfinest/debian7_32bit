1ef48c77678c0e588692e1ec12faa54ba709e1fe
paanfinest <paanfinest@gmail.com>
2017-07-05 14:43:52 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -14,12 +14,12 @@ echo -e "           (| SELAMAT DATANG DI VPS PHREAKERS JATENG OFFICIAL |)"
 echo -e "            )#Admin : Pa'an Finest     Admin : Imron Sya'faat#("
 echo -e "           ( #Admin : Affan Fattahila  Admin : Said Ramadhan # )"
 echo -e "            )#Admin : Vicky Bramantiya Admin : Aji Kurniawan #("
-echo -e "            '-------------------------------------------------'
+echo -e "            '-------------------------------------------------'"
 echo -e ""
-echo -e "1.)menu (Menampilkan Daftar Perintah)   8.)resvis (Restart Service Dropbear, Webmin Squid3, OpenVPN & SSH)"
+echo -e "1.)menu (Menampilkan Daftar Perintah)   8.)resvis (Restart Service Dropbear)"
 echo -e "2.)usernew (Membuat Akun SSH & OpenVPN) 9.)info (Menampilkan Informasi Sistem)"
 echo -e "3.)trial (Membuat Akun Trial)           10.)reboot (Reboot VPS)"
 echo -e "4.)hapus (Menghapus Akun SSH & OpenVPN) 11.)speedtest (Speedtest VPS)"
 echo -e "5.)cek (Cek User Login)                 12.)exit (Keluar Dari VPS)"
-echo -e "6.)member (Cek Member SSH & OpenVPN)
-echo -e "7.)expired (Cek User SSH & OpenVPN)
+echo -e "6.)member (Cek Member SSH & OpenVPN)"
+echo -e "7.)expired (Cek User SSH & OpenVPN)"
