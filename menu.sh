52ce54ca973f96a78f274f7d92dcf4f15868b1e8
paanfinest <paanfinest@gmail.com>
2017-07-07 00:40:31 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -12,7 +12,7 @@ echo -e "1.)menu (Menampilkan Daftar Perintah)   8.)resvis (Restart Service Drop
 echo -e "2.)usernew (Membuat Akun SSH & OpenVPN) 9.)info (Menampilkan Informasi Sistem)"
 echo -e "3.)trial (Membuat Akun Trial)           10.)reboot (Reboot VPS)"
 echo -e "4.)hapus (Menghapus Akun SSH & OpenVPN) 11.)speedtest (Speedtest VPS)"
-echo -e "5.)user login (Cek User Login)                 12.)exit (Keluar Dari VPS)"
+echo -e "5.)user login (Cek User Login)          12.)exit (Keluar Dari VPS)"
 echo -e "6.)member (Cek Member SSH & OpenVPN)"
 echo -e "7.)expired (Cek User SSH & OpenVPN)"
 echo -e ""
