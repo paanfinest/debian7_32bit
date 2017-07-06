2495a1588dd5de1eb10d3b33a8841a46cb06751b
paanfinest <paanfinest@gmail.com>
2017-07-07 00:32:51 +0700

    Update debian_7.sh

--- debian_7.sh
+++ debian_7.sh
@@ -137,7 +137,7 @@ wget -O menu "https://raw.githubusercontent.com/paanfinest/debian7_32bit/master/
 wget -O usernew "https://raw.githubusercontent.com/paanfinest/debian7_32bit/master/usernew.sh" 
 wget -O trial "https://raw.githubusercontent.com/paanfinest/debian7_32bit/master/trial.sh" 
 wget -O hapus "https://raw.githubusercontent.com/paanfinest/debian7_32bit/master/hapus.sh" 
-wget -O cek "https://raw.githubusercontent.com/paanfinest/debian7_32bit/master/user-login.sh" 
+wget -O user login "https://raw.githubusercontent.com/paanfinest/debian7_32bit/master/user-login.sh" 
 wget -O member "https://raw.githubusercontent.com/paanfinest/debian7_32bit/master/user-list.sh" 
 wget -O resvis "https://raw.githubusercontent.com/paanfinest/debian7_32bit/master/resvis.sh" 
 wget -O speedtest "https://raw.githubusercontent.com/ForNesiaFreak/FNS_Debian7/fornesia.com/null/speedtest_cli.py"
@@ -145,11 +145,14 @@ wget -O info "https://raw.githubusercontent.com/paanfinest/debian7_32bit/master/
 wget -O expired "https://raw.githubusercontent.com/paanfinest/debian7_32bit/master/user-expired.sh" 
 echo "0 0 * * * root /usr/bin/reboot" > /etc/cron.d/reboot 
 echo "* * * * * service dropbear restart" > /etc/cron.d/dropbear 
+echo "0 0 * * * root /root/user-expired.sh" > /etc/cron.d/user-expired
+#echo "@reboot root /root/userlimit.sh" > /etc/cron.d/userlimit
+#sed -i '$ i\screen -AmdS check /root/autokill.sh' /etc/rc.local
 chmod +x menu 
 chmod +x usernew 
 chmod +x trial 
 chmod +x hapus 
-chmod +x cek 
+chmod +x user login
 chmod +x member 
 chmod +x resvis 
 chmod +x speedtest 
